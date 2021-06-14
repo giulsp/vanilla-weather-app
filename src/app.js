e7c1d684c57592ec7015fd70bb23c56b1e342685
giulsp <presa.giulia@gmail.com>
2021-06-14 17:55:44 +0200

    Added real data (city, temperature, weather description)
    From OpenWeatherMap

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 //comment
+function displayTemperature (response) {
+  console.log (response.data);
+
+  let temperatureElement = document.querySelector ("#temperature");
+  let cityElement = document.querySelector ("#city");
+  let description = document.querySelector ("#description");
+  
+
+  temperatureElement.innerHTML = Math.round (response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "50d24603e09f450a85b01b98d6805e3d";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
