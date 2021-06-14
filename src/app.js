6fafcf431d5586f74dc5b6fc20677e82fd106129
giulsp <presa.giulia@gmail.com>
2021-06-14 19:39:55 +0200

    Added correct icon from the API

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 //comment
 function formatDate (timestamp){
-let date = new Date (timestamp);
+  let date = new Date (timestamp);
 
-let hours = date.getHours ();
-if (hours < 10) {
-  hours = `0${hours}`;
-} 
+  let hours = date.getHours ();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    } 
 
-let minutes = date.getMinutes ();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-} 
+  let minutes = date.getMinutes ();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    } 
 
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let day = days [date.getDay ()];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days [date.getDay ()];
 
-return `${day} ${hours}:${minutes}`;
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature (response) {
@@ -24,7 +24,8 @@ function displayTemperature (response) {
   let descriptionElement = document.querySelector ("#description");
   let humidityElement = document.querySelector ("#humidity");
   let windElement = document.querySelector ("#wind");
-  let dateElement =document.querySelector ("#date");
+  let dateElement = document.querySelector ("#date");
+  let iconElement = document.querySelector ("#icon");
 
   temperatureElement.innerHTML = Math.round (response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -32,7 +33,9 @@ function displayTemperature (response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round (response.data.wind.speed);
   dateElement.innerHTML = formatDate (response.data.dt *1000);
-}
+  iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute ("alt", response.data.weather[0].description);
+  }
 
 let apiKey = "50d24603e09f450a85b01b98d6805e3d";
 let city = "Valencia";
