62b29dc4b8fb03976b46b996c6187ae1458070cd
giulsp <presa.giulia@gmail.com>
2021-06-21 12:25:22 +0200

    Removed console.log

--- src/app.js
+++ src/app.js
@@ -19,11 +19,11 @@ function formatDate (timestamp){
 }
 
 function getForecast (coordinates) {
-  console.log (coordinates);
+  
   let apiKey = "50d24603e09f450a85b01b98d6805e3d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
   
-  console.log (apiUrl);
+  
   axios.get (apiUrl).then(displayForecast);
 }
 
